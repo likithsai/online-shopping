2432bba0a75791859d345d6fb1ce341b1f679940
likith.sai <likith.sai.ext@boehringer-ingelheim.com>
2022-07-06 14:24:51 +0530

    Added logic to put config changes to files, Minor UI improvement

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     //  check for config files
     //  if config file exist, navigate to homepage
     //  if not, navigate to install script
-    if(file_exists('config/config.php')) {
+    if(file_exists('src/config/config.php')) {
         echo 'config file exist';
     } else {
         header('location: src/install/');
