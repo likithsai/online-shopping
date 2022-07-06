fd7060e03cbab0e83d22de7026cf44d3c3fd16e6
likith.sai <likith.sai.ext@boehringer-ingelheim.com>
2022-07-06 12:36:26 +0530

    Added install script logic

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    echo "PHP string working";
+    //  check for config files
+    //  if config file exist, navigate to homepage
+    //  if not, navigate to install script
+    if(file_exists('config/config.php')) {
+        echo 'config file exist';
+    } else {
+        header('location: src/install/');
+    }
 ?>
---
+++ src/install/index.php
@@ -0,0 +1,7 @@
+<?php
+    //  Installer script
+    //  install neccessary tables required for this application to work
+
+    echo 'install script here';
+
+?>
