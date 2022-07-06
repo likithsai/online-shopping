bc2a6900116f26b873e6f0743e55bee14304a3a5
likith.sai <likith.sai.ext@boehringer-ingelheim.com>
2022-07-06 13:20:32 +0530

    Added boostrap and bootstrap-icon lib, Minor Ui improvement

--- src/install/index.php
+++ src/install/index.php
@@ -1,7 +1,23 @@
 <?php
     //  Installer script
     //  install neccessary tables required for this application to work
-
-    echo 'install script here';
-
+    require '../component/header.php';
+    echo '
+    <div class="container accordion accordion-flush my-4" id="accordionExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingOne">
+        <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <i class="bi bi-life-preserver me-2"></i>
+        <span>Install</span>
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <strong>This is the first item\'s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It\'s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </div>
+      </div>
+    </div>
+  </div>
+    ';
+    include '../component/footer.php';
 ?>
