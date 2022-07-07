cd6cee8bdfd4fd89b7a83812b329cf633417a6e4
likith.sai <likith.sai.ext@boehringer-ingelheim.com>
2022-07-07 10:49:34 +0530

    Added minor code improvement, Added class for MYSQL wrapper

--- install/index.php
+++ install/index.php
@@ -10,14 +10,13 @@
 
     // file_put_contents('../config/config.php', $var);
 
-
     //  check if config file exist or not
     if(!file_exists('../config/config.php')) {
         echo '
             <div class="container my-4 card shadow-sm border-0 rounded-0">
                 <div class="card-body">
                     <form>
-                        <div class="alert alert-primary" role="alert">
+                        <div class="alert alert-warning" role="alert">
                             <p class="fw-bold m-0">
                                 <i class="bi bi-server me-1"></i>
                                 <span>DATABASE SETUP</span>
@@ -50,21 +49,28 @@
                             <input type="text" class="form-control" id="inp_tblprefix" name="inp_tblprefix" aria-describedby="table prefix" value="tbl-">
                             <div id="emailHelp" class="form-text">If you want to run multiple installation in a single database, change this.</div>
                         </div>
-                        <div class="alert alert-primary mt-5" role="alert">
+                        <div class="alert alert-warning mt-5" role="alert">
                             <p class="fw-bold m-0">
                                 <i class="bi bi-person-circle me-1"></i>
                                 <span>ADMIN USER SETUP</span>
                             </p>
                         </div>
                         <div class="mb-3 w-100">
-                            <label for="exampleInputEmail1" class="form-label">Admin Username</label>
+                            <label for="exampleInputEmail1" class="form-label">Username</label>
                             <input type="text" class="form-control" id="inp_adminuser" name="inp_adminuser" aria-describedby="Admin username">
                             <div id="emailHelp" class="form-text">Username can have only alphanumeric characters, spaces, underscore, hyphens, period and the @ symbol.</div>
                         </div>
-                        <div class="mb-3 w-100">
-                            <label for="exampleInputEmail1" class="form-label">Database Password</label>
-                            <input type="password" class="form-control" id="inp_dbserver" name="inp_dbserver" aria-describedby="Admin password">
-                            <div id="emailHelp" class="form-text">Your database password</div>
+                        <div class="d-md-flex">
+                            <div class="mb-3 w-100 me-2">
+                                <label for="exampleInputEmail1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="inp_dbserver" name="inp_dbserver" aria-describedby="Admin password">
+                                <div id="emailHelp" class="form-text">Your database password</div>
+                            </div>
+                            <div class="mb-3 w-100">
+                                <label for="exampleInputEmail1" class="form-label">Confirm Password</label>
+                                <input type="password" class="form-control" id="inp_confirm_pwd" name="inp_confirm_pwd" aria-describedby="confirm password">
+                                <div id="emailHelp" class="form-text">Please confirm password</div>
+                            </div>
                         </div>
                         <div class="mb-3 w-100">
                             <label for="exampleInputEmail1" class="form-label">Email ID</label>
