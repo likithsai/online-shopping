64e8d5051cadd9bc3611375e0333c877c83f22fe
likith.sai <likith.sai.ext@boehringer-ingelheim.com>
2022-07-07 11:00:42 +0530

    Minor code improvement

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+
     error_reporting(E_ALL);
+    include 'class/MySQLiDB.class.php';
+    //  check for config files exits or not
+    //  if exist, then include in the file
+    if(file_exists('config/config.php')) {
+        require_once 'config/config.php';
+    }
+
     require 'component/header.php';
     
     //  check if config file exist or not
